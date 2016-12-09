c54888e379ac1bfcc7614f7c12e3add2738abddd
Aaron Ambrose <ambroseaaron@live.com>
2016-12-08 18:07:41 -0600

    Create ColorChooserTest.java

---
+++ RGB-Color-Chooser/ColorChooserTest.java
@@ -0,0 +1,9 @@
+/**
+ * Tests the ColorChooser class
+ * @author Aaron Ambrose
+ */
+public class ColorChooserTest {
+    public static void main(String arg[]) {
+        new ColorChooser();
+    }
+}
