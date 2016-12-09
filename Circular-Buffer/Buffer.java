366ff284da82ec5e5956f984a40258b783732c83
Aaron Ambrose <ambroseaaron@live.com>
2016-12-08 18:09:01 -0600

    Create Buffer.java

---
+++ Circular-Buffer/Buffer.java
@@ -0,0 +1,9 @@
+/**
+ * Given buffer interface from Java How to Program
+ */
+public interface Buffer {
+
+    void set( int value ); // place int value into Buffer
+
+    int get(); // return int value from Buffer
+} // end interface Buffer
